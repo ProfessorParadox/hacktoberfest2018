3bb90cfea806711e9df590054ba3436aaa9861fe
Arpitsing <43248100+Arpitsing@users.noreply.github.com>
2018-10-04 13:23:27 +0530

    Update fibonacci.cpp

--- fibonacci/fibonacci.cpp
+++ fibonacci/fibonacci.cpp
@@ -3,9 +3,11 @@
 using namespace std;
 int fib(int n)
 {
-   if (n <= 1)
-      return n;
-   return fib(n-1) + fib(n-2);
+   if (n >= 1)
+      return n*fib(n-1);
+   else
+      return 1;
+
 }
 
 int main ()
